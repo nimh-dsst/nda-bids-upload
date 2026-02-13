3fb5bf531258c32f4288e713e1ac77a1d885d7d0
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2026-02-13 13:48:07 -0500

    apply code suggestions from review

--- utilities/lookup.py
+++ utilities/lookup.py
@@ -41,32 +41,28 @@ class LookUpTable:
             except (FileExistsError, FileNotFoundError, json.JSONDecodeError) as err:
                 raise err
 
-       
-        age_multiplier = 1
-        # check to see what units are in age
-        if "y" in str.lower(self.participants_json.get("age", {}).get("Units", "")):
-            age_multiplier = 12
-        elif "m" in str.lower(self.participants_json.get("age", {}).get("Units", "")):
-            age_multiplier = 1
-        elif "w" in str.lower(self.participants_json.get("age", {}).get("Units", "")):
-            age_multiplier = 1 / 4
-        else:
-            print(
-                f"unable to determine participant.age.Units from "
-                f"{self.bids_layout.get(suffix='participants', extension='json')}, "
-                "not converting to months."
-            )
 
-        # check recast gender column and sidecar as sex if gender
-        gender_col = next(
+        # convert ages from years to months per NDA requirements
+        age_multiplier = 12
+        
+        # recast gender column and sidecar as sex if gender is present
+        gender_col, sex_column = next(
             (
                 column
                 for column in self.participants_tsv.columns
                 if column.lower() == "gender"
             ),
             None,
+        ), next(
+            (
+                column
+                for column in self.participants_tsv.columns
+                if column.lower() == "sex"
+            ),
+            None,
         )
-        if gender_col:
+
+        if gender_col and not sex_column:
             self.participants_tsv = self.participants_tsv.rename(columns={gender_col: "sex"})
             self.participants_json["sex"] = self.participants_json.pop(gender_col)
 
